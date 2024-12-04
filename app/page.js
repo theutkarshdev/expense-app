807b3a0d274beb94913b010652d4a2e13ef13fab
Utkarsh Kushwaha <utkarsh@radiantinfonet.com>
2024-12-04 16:44:06 +0530

    button added

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="h-dvh w-full bg-white flex flex-col justify-center px-3">
-      <h1 className="text-5xl font-bold text-center">Home</h1>
+      <h1 className="text-5xl font-bold text-center mb-4">Home</h1>
+      <Link href={"/dashboard"}>
+        <Button className="w-full" size="lg">
+          Go to Dashboard
+        </Button>
+      </Link>
     </div>
   );
 }
