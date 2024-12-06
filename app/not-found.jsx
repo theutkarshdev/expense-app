2b4da72cb867fb60e029fba74b8aac48f11f95c3
Utkarsh Kushwaha <utkarsh@radiantinfonet.com>
2024-12-06 15:51:50 +0530

    done

---
+++ app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="text-center h-dvh flex flex-col items-center justify-center max-w-screen-xl px-10">
+      <h1 className="text-3xl font-extrabold mb-5 text-theme">Error - 404</h1>
+      <p>Page not found.</p>
+    </div>
+  );
+}
