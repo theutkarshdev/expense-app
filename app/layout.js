1ba2af5c7a42d7f4da51ec02dbfd9179803859ee
Utkarsh Kushwaha <utkarsh@radiantinfonet.com>
2024-12-09 09:54:37 +0530

    done

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <main className="w-full max-w-screen-sm mx-auto bg-slate-50">{children}</main>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
